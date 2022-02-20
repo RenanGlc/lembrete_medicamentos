4f507f28476a76b04aee675f3a83f36c5f28f39b
RenanPmtl <renangallucci@gmail.com>
2022-02-20 16:52:39 -0300

    Implementações
    Implementação do Firebase e do Onesignal

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:lembrete_medicamentos/HomePage.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: HomePage(),
-    debugShowCheckedModeBanner: false,
-  ));
+  WidgetsFlutterBinding.ensureInitialized();
+
+  OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+
+  OneSignal.shared.setAppId("ce09d619-4f11-41cc-9125-e8da6ddcc2ec");
+
+  runApp(
+      const MaterialApp(
+        home: HomePage(),
+        debugShowCheckedModeBanner: false,
+      )
+  );
 }
