509458d83b61f70bbe4fa271b9536b9c0e767600
RenanPmtl <renan.pimentel@grupoep.com.br>
2022-02-20 14:44:09 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:lembrete_medicamentos/HomePage.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
